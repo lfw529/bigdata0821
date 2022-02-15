a104587cb14f1a682f920396ce1cd1a1affea292
lfw529 <differentlee1012@163.com>
2022-02-15 20:37:46 +0800

    111

---
+++ Flink_0821/src/main/java/com/lfw/flink/sql/Examples_CreateTemporaryView.java
@@ -0,0 +1,9 @@
+package com.lfw.flink.sql;
+
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+
+public class Examples_CreateTemporaryView {
+    //create some DataStream
+    DataStream<Tuple2<Long, String>> dataStream =
+}
